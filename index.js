d645c064e6bd458570a95fc3621a65f0591e9e2f
Ryan Juergens <rjuergens@hotmail.com>
2023-05-08 21:04:57 -0500

    passes tests

---
+++ index.js
@@ -0,0 +1,11 @@
+function introduction(name){
+    console.log (`${name}`)
+    return `Hi, my name is ${name}.`}
+    
+function introductionWithLanguage(name, language){
+    console.log (`${name}, ${language}`)
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+function introductionWithLanguageOptional(name, language = "JavaScript"){console.log (`${name}, ${language}`)
+return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
